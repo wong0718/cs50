51e9c577bbece388970bda3a79d34d6a8e9b247a
wong0718 <wongrenzheng8868@gmail.com>
2025-11-19 14:21:15 +0800

    final

--- Week 7 (SQL)/fiftyville/log.sql
+++ Week 7 (SQL)/fiftyville/log.sql
@@ -1,4 +1,4 @@
--- Keep a log of any SQL queries you execute as you solve the mystery.
+    -- Keep a log of any SQL queries you execute as you solve the mystery.
 
 -- check crime log
 SELECT description FROM crime_scene_reports WHERE month = 7 AND day = 28 AND street ='Humphrey Street';
